569e5646cde316ef7417083d3ba03a8eb8464b69
Arthur Meibak <108312926+arthurmeibak@users.noreply.github.com>
2023-06-02 13:07:04 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/*==========Slide-in effect==========*/
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.slide-in');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+/*==========Mixitup Filter==========*/
+let mixerProjects = mixitup('.projects__container', {
+  selectors: {
+      target: '.project__item',
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+/*==========Header==========*/
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+ 
